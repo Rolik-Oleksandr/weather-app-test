279e2e8e01d53d6c7e9a24fa28f6340cf1a9d296
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-06-13 21:57:51 +0300

    Initial Commit

---
+++ weather-app-test/weather_app_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_app_testApp.swift
+//  weather-app-test
+//
+//  Created by Oleksandr Rolik on 13.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct weather_app_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
