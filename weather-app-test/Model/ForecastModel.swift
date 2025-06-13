7e2183771c1dedd31ba77e2bd6598c604d574573
Rolik-Oleksandr <rolik.alexx@gmail.com>
2025-06-14 01:45:54 +0300

    add codingKeys and handle nil responce for temp in forecast model

--- weather-app-test/Model/ForecastModel.swift
+++ weather-app-test/Model/ForecastModel.swift
@@ -1,8 +1,13 @@
 import Foundation
 
-struct WeatherResponce: Codable {
+struct ForecastResponce: Codable {
     let cityName: String
     let data: [DailyForecast]
+    
+    enum CodingKeys: String, CodingKey {
+        case cityName = "city_name"
+        case data
+    }
 }
 
 struct DailyForecast: Codable, Identifiable {
@@ -11,6 +16,20 @@ struct DailyForecast: Codable, Identifiable {
     let highTemp: Double
     let lowTemp: Double
     let weather: WeatherDescription
+    let temp: Double?
+    
+    enum CodingKeys: String, CodingKey {
+        case date = "valid_date"
+        case highTemp = "high_temp"
+        case lowTemp = "low_temp"
+        case weather
+        case temp
+    }
+    
+    var getTemp: String {
+        guard let temp = temp else { return "N/A" }
+        return "\(temp)`C"
+    }
 }
 
 struct WeatherDescription: Codable {
